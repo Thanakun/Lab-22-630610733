d07fe2861e23be33b5451c6fdcfe3c5da18e1279
Thanakun <Thanakun_Ch@cmu.ac.th>
2021-03-12 16:41:50 +0700

    Yeah Please be mercy

--- lab22_1.cpp
+++ lab22_1.cpp
@@ -64,69 +64,20 @@ void List::append(int d){
 }
 
 //Write List::remove() here
-#include<iostream>
-using namespace std;
 
-
-class Node{
-    public:
-    	int data;
-    	Node *next;
-    	~Node();
-};
-
-class List{
-	public:
-		Node *root;
-		int size;
-		void show();
-		void append(int);
-		void insert(int,int);
-		void remove(int);
-};
-
-Node::~Node(){
-    cout << data << " was deleted.\n";
-}
-
-void List::insert(int d,int idx){	
-	Node *n = new Node;
-	n->data = d;
-	
-	if(idx == 0){
-		n->next = root;
-		root = n;
-		return;
+void List::remove(int k){
+	Node *a = root;
+	Node *b = root;
+	for(int i = 0; i < k+1;i++){
+		a = a->next;
 	}
-	Node *current = root;
-	for(int i = 0; i < idx-1;i++){
-		current = current->next;
+	for(int i = 0;i < k;i++){
+		b = b->next;
 	}
-	n->next = current->next;
-	current->next = n;		
+	Node *temp = a->next;
+	int tempdata= a->data;
+	delete b;
+	b->data = tempdata;
+	b->next = temp;
+	size--;
 }
-
-void List::show(){
-	Node *current = root;
-	cout << current->data << " ";	
-	while(current->next){
-		current = current->next;
-		cout << current->data << " ";
-	}	
-}
-
-void List::append(int d){	
-	Node *n = new Node;
-	n->data = d; n->next = NULL;
-	if(root == NULL) root = n;
-	else{
-		Node *current = root;
-		while(current->next){
-			current = current->next;
-		}
-		current->next = n;
-	}
-	size++;
-}
-
-//Write List::remove() here
--- lab22_2.cpp
+++ lab22_2.cpp
@@ -16,6 +16,7 @@ int main(){
 	
 	list<string> line_up;
 	list<string>::iterator loc;
+	list<string>::iterator loc1;
 	
 	line_up.push_back("Alice");
 	line_up.push_back("Bob");
@@ -24,7 +25,20 @@ int main(){
 	line_up.insert(loc,"Oscar");
 	
 	//Write your code here
-	
+	line_up.push_back("Luffy");
+	line_up.push_back("Sanji");
+	line_up.push_back("Nami");
+
+	line_up.pop_front();
+	line_up.pop_front();
+	loc1 = find(line_up.begin(),line_up.end(),"Sanji");
+	line_up.insert(loc1,"Narutu");
+	line_up.push_front("Prayuth");
+	line_up.insert(loc,"Tony");
+	line_up.erase(loc);
+	line_up.pop_front();
+	line_up.pop_front();
+	line_up.pop_front();
 	printList(line_up);
 		
 	return 0;
